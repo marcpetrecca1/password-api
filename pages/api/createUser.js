3c04fae3bd4ec2c5288f9f8b50e26a1e7a6f6fed
MarcAnthony Petrecca <marcanthony.petrecca@gmail.com>
2022-10-12 19:30:22 -0400

    createUser route complete

--- pages/api/createUser.js
+++ pages/api/createUser.js
@@ -1,87 +1,69 @@
 import prisma from '@/lib/prisma';
 
 export default async function handler(req, res) {
-  // if (req.method !== 'POST') {
-  //   res.status(405).json({
-  //     message: 'Method Not Allowed',
-  //   });
-  //   return;
-  // }
+  if (req.method !== 'POST') {
+    res.status(405).json({
+      message: 'Method Not Allowed',
+    });
+    return;
+  }
   const body = req.body;
 
   try {
-    // const newEntry = await prisma.users.create({
-    //   data: body,
-    // });
-    // return res.json(newEntry).status(200);
-    const entries =
-      await prisma.$queryRaw`DELETE FROM users WHERE email='michael@gmail.com'`;
-    return res.json(entries).status(200);
+    // const entries = await prisma.password.findMany();
+    // await prisma.password.deleteMany();
+    // await prisma.users.deleteMany();
+    const passwordValidation = validatePassword(body.password);
+    if (typeof passwordValidation === 'boolean') {
+      const entries = await prisma.users.create({
+        data: {
+          email: body.email,
+          passwords: {
+            create: {
+              password: body.password,
+            },
+          },
+        },
+      });
+      return res.status(200).json({ message: 'Created', success: true });
+    } else {
+      if (typeof passwordValidation !== 'boolean') {
+        return res.status(500).json({
+          error: 'Error creating question',
+          success: false,
+          data: passwordValidation,
+        });
+      }
+    }
   } catch (error) {
     console.error('Request error', error);
-    res.status(500).json({ error: 'Error creating question', success: false });
-  }
-  // const { email, password } = body;
-  // const emailValidation = validateEmail(email);
-  // const passwordValidation = validatePassword(password);
-
-  // console.log('emailValidation', emailValidation);
-  // console.log('passwordValidation', passwordValidation);
-
-  // if (emailValidation) {
-  //   return res.status(400).json({
-  //     message: 'There is already an account associated with this email',
-  //   });
-  // }
-  // if (!passwordValidation) {
-  //   return res.status(400).json({
-  //     message: passwordValidation,
-  //   });
-  // }
-  // res.status(200).json({
-  //   message: 'You successfully created an account',
-  //   success: true,
-  //   body: body,
-  // });
-}
-
-async function validateEmail(email) {
-  try {
-    const response = await prisma.users.findFirst({
-      where: {
-        email: email,
-      },
-    });
-    if (response === null) {
-      return true;
-    }
-    console.log('this is the response', response);
-  } catch (error) {
-    console.log('this is the error', error);
-    return error;
+    res
+      .status(500)
+      .json({ error: 'This email is already in use', success: false });
   }
 }
 
 function validatePassword(password) {
   const errorStorage = {};
-  const capitalLetters = /^A-Z/;
-  const specialCharacters = /^\W|_/g;
-  const numbers = /^\d/;
-  const lowercaseLetters = /^a-z/;
+  const capitalLetters = /[A-Z]/g;
+  const specialCharacters = /[\W|_]/g;
+  const numbers = /[\d]/g;
+  const lowercaseLetters = /[a-z]/g;
+
   if (password.length < 8) {
     errorStorage.length = 'Password must have atleast 8 characters';
   }
-  if (capitalLetters.test(password)) {
+  if (password.match(capitalLetters) === null) {
     errorStorage.capitalLetters = 'You must have atleast 1 capital letter';
   }
-  if (specialCharacters.test(password)) {
+  if (password.match(specialCharacters) === null) {
     errorStorage.specialCharacters =
       'You must have atleast one special character';
   }
-  if (numbers.test(password)) {
+  if (password.match(numbers) === null) {
     errorStorage.numbers = 'You must have atleast one number';
   }
-  if (lowercaseLetters.test(password)) {
+  if (password.match(lowercaseLetters) === null) {
     errorStorage.lowerCase = 'You must have atleast one lowercase number';
   }
 
