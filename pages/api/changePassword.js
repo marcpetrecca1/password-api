28a0db9a09bfd2d75f35e273f172b1e172f4e816
MarcAnthony Petrecca <marcanthony.petrecca@gmail.com>
2022-10-13 22:53:49 -0400

    Everything works except delete in changePassword

--- pages/api/changePassword.js
+++ pages/api/changePassword.js
@@ -63,7 +63,6 @@ export default async function handler(req, res) {
       return res.status(400).json({
         error: 'Cannot use any previous passwords',
         success: false,
-        body: present,
       });
     }
     const passwordList = passwords.passwords;
