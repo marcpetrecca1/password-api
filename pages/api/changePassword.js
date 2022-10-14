57f833e4fd4c236e9f29b7b5c0cdfa74e24bcb41
MarcAnthony Petrecca <marcanthony.petrecca@gmail.com>
2022-10-14 10:11:45 -0400

    Ready for review

--- pages/api/changePassword.js
+++ pages/api/changePassword.js
@@ -15,6 +15,7 @@ export default async function handler(req, res) {
 
   try {
     // check if account is present
+
     const passwords = await prisma.users.findUnique({
       where: {
         email: email,
@@ -44,6 +45,7 @@ export default async function handler(req, res) {
     }
 
     // check to see if new password is same as any old
+
     const present = await prisma.password.findMany({
       where: {
         AND: [
@@ -65,24 +67,22 @@ export default async function handler(req, res) {
         success: false,
       });
     }
+
     const passwordList = passwords.passwords;
+
     // find the active password and oldest
 
     let activePassword;
-    let oldest = new Date(passwordList[0].expiresOn).getTime();
 
     for (let i = 0; i < passwordList.length; i++) {
       let currentPassword = passwordList[i];
-      let currentExpiration = new Date(currentPassword.expiresOn).getTime();
       if (currentPassword.activePassword === true) {
         activePassword = currentPassword;
-      }
-      if (currentExpiration < oldest) {
-        oldest = currentPassword;
       }
     }
 
     // change current active password to false
+
     const updateCurrent = await prisma.password.update({
       where: {
         id: activePassword.id,
@@ -92,9 +92,10 @@ export default async function handler(req, res) {
       },
     });
 
-    // if there are 3 previous passwords, remove the oldest
+    // if length is 4 passwords long remove oldest
 
     if (passwordList.length === 4) {
+      let oldest = passwordList[0];
       const removeOldest = await prisma.password.delete({
         where: {
           id: oldest.id,
--- pages/api/createUser.js
+++ pages/api/createUser.js
@@ -49,7 +49,7 @@ export default async function handler(req, res) {
         });
       }
     }
-    // return res.status(200).json({ message: 'done', success: true });
+    // return res.status(200).json({ message: passwords, success: true });
   } catch (error) {
     console.error('Request error', error);
     res
